4ad4dcfadf2d1008324a7ed6eb9d713fa38564ec
HoseaKim1025 <hoseakim1025@gmail.com>
2024-10-30 13:53:53 +0900

    init

---
+++ my-app/src/App.tsx
@@ -0,0 +1,18 @@
+// src/App.tsx
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import MainPage from './pages/MainPage';
+
+const App: React.FC = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/main" element={<MainPage />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
